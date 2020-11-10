73b143f2a77c0a70d5edf811173a57c8bf15bc09
Roman Sivakov <romansivakov@gmail.com>
2020-11-10 18:47:32 +0300

    переключалка

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -128,7 +128,7 @@ const NETWORK_LABELS: { [chainId in ChainId]: string | null } = {
   [ChainId.ROPSTEN]: 'Ropsten',
   [ChainId.GÖRLI]: 'Görli',
   [ChainId.KOVAN]: 'Kovan',
-  [ChainId.BNB]: 'Binance',
+  // [ChainId.BNB]: 'Binance',
   // [ChainId.BSCNET]: 'Chapel'
 }
 
